dd6da8a5ffdd0aa831a6807056c3f1f9d2388ba6
pradhumn panchal <38205334+pradhumnpanchal@users.noreply.github.com>
2019-07-08 12:52:44 +0530

    Add files via upload

---
+++ finalProject.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+data = pd.read_csv("train.csv")
+xtrain = data.drop('subscribed',axis = 1)
+xtrain = pd.get_dummies(xtrain)
+ytrain = data['subscribed'] 
+logreg = LogisticRegression()
+logreg.fit(xtrain,ytrain)
+test = pd.read_csv("test.csv")
+test = pd.get_dummies(test)
+pred = logreg.predict(test)
+test['subscribed'] = pred
+test.to_csv('new.csv')
